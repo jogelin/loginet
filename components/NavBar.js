dc112389680906dfbb9685a4dbaf1d0dea7e406f
Aline <aline@MacBook-Pro-de-Aline.local>
2016-06-22 13:25:29 +0200

    blabla

--- components/NavBar.js
+++ components/NavBar.js
@@ -1,20 +1,24 @@
 import React from 'react';
 //import MAppBar from 'material-ui/AppBar';
 
-const NavBar = () => (
-  <nav className="transparent">
-    <a href="#" className="brand">
-      <img className="logo" src="/assets/img/logo.png" />
-    </a>
+class NavBar extends React.Component {
+  render() {
+    return (
+      <nav className="transparent">
+        <a href="#" className="brand">
+          <img className="logo" src="/assets/img/logo.png" />
+        </a>
 
-    <input id="bmenub" type="checkbox" className="show" />
-    <label for="bmenub" className="burger pseudo button">menu</label>
+        <input id="bmenub" type="checkbox" className="show" />
+        <label for="bmenub" className="burger pseudo button">menu</label>
 
-    <div className="menu">
-      <a href="#" className="button">Demo</a>
-      <a href="#">Plugins</a>
-    </div>
-  </nav>
-);
+        <div className="menu">
+          <a href="#" className="button">Demo</a>
+          <a href="#">Plugins</a>
+        </div>
+      </nav>    );
+  }
+
+};
 
 export default NavBar;
